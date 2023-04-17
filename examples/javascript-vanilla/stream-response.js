b583f8c9f425ad8a6bc7086777ce5ca6b59791c3
Vishwas <vishwas.g5@gmail.com>
2023-04-17 18:52:05 +0530

    Read the response as a stream of data

---
+++ examples/javascript-vanilla/stream-response.js
@@ -0,0 +1,109 @@
+/**
+ * This code demonstrates how to use the OpenAI API to generate chat completions.
+ * The generated completions are received as a stream of data from the API and the
+ * code includes functionality to handle errors and abort requests using an AbortController.
+ * The API_KEY variable needs to be updated with the appropriate value from OpenAI for successful API communication.
+ */
+
+const API_URL = "https://api.openai.com/v1/chat/completions";
+const API_KEY = "YOUR_API_KEY";
+
+const promptInput = document.getElementById("promptInput");
+const generateBtn = document.getElementById("generateBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resultText = document.getElementById("resultText");
+
+let controller = null; // Store the AbortController instance
+
+const generate = async () => {
+  // Alert the user if no prompt value
+  if (!promptInput.value) {
+    alert("Please enter a prompt.");
+    return;
+  }
+
+  // Disable the generate button and enable the stop button
+  generateBtn.disabled = true;
+  stopBtn.disabled = false;
+  resultText.innerText = "Generating...";
+
+  // Create a new AbortController instance
+  controller = new AbortController();
+  const signal = controller.signal;
+
+  try {
+    // Fetch the response from the OpenAI API with the signal from AbortController
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: promptInput.value }],
+        max_tokens: 100,
+        stream: true, // For streaming responses
+      }),
+      signal, // Pass the signal to the fetch request
+    });
+
+    // Read the response as a stream of data
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+    resultText.innerText = "";
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      // Massage and parse the chunk of data
+      const chunk = decoder.decode(value);
+      const lines = chunk.split("\n");
+      const parsedLines = lines
+        .map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
+        .filter((line) => line !== "" && line !== "[DONE]") // Remove empty lines and "[DONE]"
+        .map((line) => JSON.parse(line)); // Parse the JSON string
+
+      for (const parsedLine of parsedLines) {
+        const { choices } = parsedLine;
+        const { delta } = choices[0];
+        const { content } = delta;
+        // Update the UI with the new content
+        if (content) {
+          resultText.innerText += content;
+        }
+      }
+    }
+  } catch (error) {
+    // Handle fetch request errors
+    if (signal.aborted) {
+      resultText.innerText = "Request aborted.";
+    } else {
+      console.error("Error:", error);
+      resultText.innerText = "Error occurred while generating.";
+    }
+  } finally {
+    // Enable the generate button and disable the stop button
+    generateBtn.disabled = false;
+    stopBtn.disabled = true;
+    controller = null; // Reset the AbortController instance
+  }
+};
+
+const stop = () => {
+  // Abort the fetch request by calling abort() on the AbortController instance
+  if (controller) {
+    controller.abort();
+    controller = null;
+  }
+};
+
+promptInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    generate();
+  }
+});
+generateBtn.addEventListener("click", generate);
+stopBtn.addEventListener("click", stop);
